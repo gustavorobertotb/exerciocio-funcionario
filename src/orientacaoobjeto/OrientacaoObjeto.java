c662dc546ed7660aacf2d979f60961b363f909ab
gustavorobertotb <gustavo.souza97@outlook.com>
2021-04-16 11:46:50 -0300

    segundo commit

--- src/orientacaoobjeto/OrientacaoObjeto.java
+++ src/orientacaoobjeto/OrientacaoObjeto.java
@@ -6,14 +6,7 @@ public class OrientacaoObjeto {
       
         Funcionario f1 = new Funcionario("Gustavo", "Educação", 15000, "14n123m", "12/01/2012");
           
-        System.out.println(f1.getNome());
-        System.out.println(f1.getDepartamento());
-        System.out.println(f1.getSalario());
-        System.out.println(f1.getRG());
-        System.out.println(f1.getDataEntrada());
-        
-        f1.setNome("Gustavo Roberto");
-        
+       
         System.out.println(f1.getNome());
         
         f1.recebeAumento(1000);
